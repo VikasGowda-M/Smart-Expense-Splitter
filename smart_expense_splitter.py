b65b127d43a2598db3d4777ee5f84b5614101af6
VikasGowda M <vikasmanjunath2005@gmail.com>
2026-03-17 23:51:47 +0530

    Basic equal splitter

---
+++ smart_expense_splitter.py
@@ -0,0 +1,4 @@
+total_bill = float(input("Enter the total bill amount: "))
+num_people = int(input("Enter the number of people splitting the bill: "))
+per_person = total_bill/num_people
+print("Each person should pay:", per_person)
