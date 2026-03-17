c544f39227b9b0f9978a56e1eb16fb4e38498d78
VikasGowda M <vikasmanjunath2005@gmail.com>
2026-03-17 23:55:48 +0530

    Added tip feature

--- smart_expense_splitter.py
+++ smart_expense_splitter.py
@@ -1,4 +1,8 @@
 total_bill = float(input("Enter the total bill amount: "))
 num_people = int(input("Enter the number of people splitting the bill: "))
-per_person = total_bill/num_people
+tip_percentage = float(input("Enter the tip percentage (e.g., 15 for 15%): "))
+tip_amount = total_bill * (tip_percentage / 100)
+final_bill = total_bill + tip_amount
+per_person = final_bill/num_people
+print("Total bill including tip:", final_bill)
 print("Each person should pay:", per_person)
