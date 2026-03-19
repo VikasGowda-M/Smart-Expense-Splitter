a834b327cbee752399ae96b6f948610f7d6fecd8
VikasGowda M <vikasmanjunath2005@gmail.com>
2026-03-20 00:29:31 +0530

    Added new feature - discount application and warning alert is total bill excludes than budget

--- smart_expense_splitter.py
+++ smart_expense_splitter.py
@@ -1,8 +1,30 @@
+total_buget = float(input("Enter your total budget which you are willing to spend: "))
 total_bill = float(input("Enter the total bill amount: "))
 num_people = int(input("Enter the number of people: "))
 if num_people <= 0:
     print("Number of people must be greater than zero.Thank you!")
     exit()
+elif num_people == 1:
+    print("Only one person is paying the bill. No need to split. Thank you!")
+    exit()
+#discount application:
+discount_percentage = float(input("Enter the discount percentage (if any, otherwise enter 0): "))
+if discount_percentage > 0:
+    discount_amount = total_bill * (discount_percentage / 100)
+    total_bill -= discount_amount
+    print(f"Discount amount: {discount_amount}")
+elif discount_percentage < 0:
+    print("Invalid discount percentage. It cannot be negative. Thank you!")
+    exit()
+elif discount_percentage ==100:
+    total_bill = 0
+    print("Total bill after discount: 0")
+elif discount_percentage > 100:
+    print("Invalid discount percentage. It cannot be greater than 100. Thank you!")
+    exit()
+else:
+    print("No discount applied.")
+print(f"Total bill amount after discount (if any): {total_bill}")
 #tip suggestion:
 tip_percentage = 0
 tip_suggestion = input("Do you want any tip suggestions? (yes/no): ").lower()
@@ -27,6 +49,9 @@ if tip_suggestion == "yes":
     confirm = input("Is the suggested tip percentage acceptable? (yes/no): ").lower()
     if confirm == "yes":
         tip_percentage = suggested_tip
+        print(f"Tip percentage set to: {tip_percentage}%")
+        print(f"Tip amount based on suggested percentage: {total_bill * (tip_percentage / 100)}")
+        print(f"Total bill including tip: {total_bill + (total_bill * (tip_percentage / 100))}")
     else:
         tip_percentage = float(input("Enter your desired tip percentage: "))
 else:
@@ -35,7 +60,10 @@ paid_by = input("Enter the name of the person who paid the bill: ")
 tip_amount = total_bill * (tip_percentage / 100)
 final_bill = total_bill + tip_amount
 per_person = final_bill/num_people
-print("\n--- Bill Summary ---")
+print("\n--- Bill Summary ---\n")
+if final_bill > total_buget:
+    print("Warning: The total bill including tip exceeds your budget!\n\n")
+per_person = final_bill/num_people
 print("Total bill amount excluding tip:", total_bill)
 print("Tip amount:", tip_amount)
 print("Total bill including tip:", final_bill)
